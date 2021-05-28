16d40523e7cc70a2fd63a5e13cdb88f6e356786f
Embu Banjar <embubanjar16@gmail.com>
2021-05-28 22:44:22 +0700

    ada yg kelupaan le

--- lib/view_main/today.dart
+++ lib/view_main/today.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-//import 'package:weather/weather.dart';
+import 'package:weather/weather.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
